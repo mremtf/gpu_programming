803a1f1c9399ff33a08abb5be8ffad50639c0456
Will Starms <vilhelmen@gmail.com>
2016-02-09 15:27:08 -0600

    Ideas

--- project_1/include/device_queries.hpp
+++ project_1/include/device_queries.hpp
@@ -1,7 +1,13 @@
-#ifndef _DEICE_QUERIES_HPP__
-#define _DEICE_QUERIES_HPP__
+#ifndef _DEVICE_QUERIES_HPP__
+#define _DEVICE_QUERIES_HPP__
 
+#include <vector>
 // Functions to query device count and the global memory of a device
+
+std::vector<int> get_devices();
+
+unsigned long get_global_mem(int device_id);
+
 
 
 #endif
