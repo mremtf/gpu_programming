5f62ef025f0adf4a1af47b2583fa0d1a6a2ffce6
Will Starms <vilhelmen@gmail.com>
2016-02-09 20:42:15 -0600

    device query stuff

--- project_1/include/device_queries.hpp
+++ project_1/include/device_queries.hpp
@@ -1,12 +1,29 @@
 #ifndef _DEVICE_QUERIES_HPP__
 #define _DEVICE_QUERIES_HPP__
 
+#include <cuda_runtime_api.h>
 #include <vector>
 // Functions to query device count and the global memory of a device
 
-std::vector<int> get_devices();
+std::vector<int> get_devices() {
+    std::vector<int> devices;
+    int device_count = 0;
+    if (cudaGetDeviceCount(&device_count) == CudaSuccess && device_count) {
+        for (int i = 0; i < device_count; ++i) {
+        	devices.push_back(i);
+        }
+        return devices;
+    } else {
+        throw std::runtime_error("No device and/or cuda freakout!");
+    }
+}
 
-unsigned long get_global_mem(int device_id);
+size_t get_global_mem(const int device_id) {
+    cudaDeviceProp prop_struct;
+    if (cudaGetDeviceProperties(&prop_struct, device_id) == CudaSuccess) {
+        return prop_struct.totalGlobalMem;
+    }
+}
 
 
 
--- project_1/src/vector_main.cpp
+++ project_1/src/vector_main.cpp
@@ -1,4 +1,5 @@
 #include "../include/parameters.hpp"
+#include "../include/device_queries.hpp"
 #include "../include/timer.hpp"
 
 int main(int argc, char **argv) {
@@ -7,4 +8,10 @@ int main(int argc, char **argv) {
     options_t prog_opts = process_params(argc, argv);
     a.end();
     std::cout << "Took " << a.ms_elapsed() << std::endl;
+
+    auto devices = get_devices();
+    std::cout << "Got " << devices.size() << " devices.\n";
+    for (const auto i : devices) {
+        std::cout << "Device " << i << " Global mem: " << get_global_mem(i) << std::endl;
+    }
 }
