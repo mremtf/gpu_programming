912d3b2db2686ef508258e505cafab492e1ea7c2
mremtf <matt.r.england@gmail.com>
2016-02-15 15:51:10 -0600

    new update

--- project_1/src/transpose.cu
+++ project_1/src/transpose.cu
@@ -20,13 +20,32 @@ bool cpu_transpose(const std::vector<float>& in, const std::vector<float>& out, 
 	return true;
 }
 
-__global__ transpose_global (const float* in_matrix, float* out_matrix, const size_t rows, const size_t cols) {
-	const size_t x = blockIdx.x * TILE_DIM + threadIdx.x;
-  const size_t y = blockIdx.y * TILE_DIM + threadIdx.y;
-	const width = gridDim.x * TILE_DIM;
+__global__ transpose_global (float *in, float *out, const unsigned W, const unsigned step, 
+		const unsigned total, const unsigned fix_position, unsigned fix_step) {
 
-		  for (int j = 0; j < TILE_DIM; j+= BLOCK_ROWS)
-				    odata[(y+j)*width + x] = idata[(y+j)*width + x];
+	  unsigned position = blockDim.x * blockIdx.x + threadIdx.x;
+    position *= step;
+    // printf("%d\t%d\t%d\t%d\n", blockDim.x, blockIdx.x, threadIdx.x, position);
+    // This is a really dumb edge case clearly used to break the code, but
+    // hell if I'm missing points for not catching when you request more threads than elements!
+    if (position < total) {
+        // Interesting thing to test
+        // ternary here vs if. Only divergence should be last warp in last block
+        // But the ternary will probably slow down everything?
+        // It would avoid a warp divergence, though!
+        if (position == fix_position) {
+            step = fix_step;
+        }
+        in += position;
+				unsigned y = 0; //floor( (float) position/ (float)W);
+				unsigned x = 0; //position - (y * W);  
+        for (int i = 0; i < step; ++i, ++position) {
+            // printf("%p %p %i %i %f %f\n", a, b, position, i, *a, *b);
+						unsigned y = floor( (float) position/ (float)W);
+						unsigned x = position - (y * W); 
+            out[x * W + y] += *in;
+        }
+    }
 }
 
 __global__ transpose_shared(float* in_matrix, float* out_matrix, size_t rows, size_t cols) {
@@ -46,6 +65,58 @@ void launch_kernels_and_report(const options_t &opts) {
         throw std::runtime_error("Thread/Block count of 0!");
     }
 
+		float mem = get_global_mem(devices[i]) / sizeof(float) * util / 2.0;
+
+		timer gpu_total, gpu_execute;
+
+		std::cout << "Dev: " << config[i].device << " Step: " << config[i].step << " Fix_P: " << config[i].fix_position
+              << " Fix_s: " << config[i].fix_step << " Threads: " << thread_total
+              << " Val total: " << float_vec_size[i] << std::endl;
+
+    std::vector<float> in = generate_vector(float_vec_size[i]);
+   	std::vector<float> out = generate_vector(float_vec_size[i]);
+    std::vector<float> c = std::vector<float>(float_vec_size[i]);
+
+    if (cudaSetDevice(config[i].device) != cudaSuccess) {
+    	throw std::runtime_error("could not select device!");
+    }
+
+    gpu_total.begin();
+
+    if (cudaMalloc(&config[i].vec_a_device, float_vec_size[i] * sizeof(float)) != cudaSuccess
+    			|| cudaMalloc(&config[i].vec_b_device, float_vec_size[i] * sizeof(float)) != cudaSuccess) {
+        throw std::runtime_error("Failed to malloc vector!");
+    }
+
+    if (cudaMemcpy(config[i].vec_a_device, a.data(), float_vec_size[i] * sizeof(float), cudaMemcpyHostToDevice)
+    		!= cudaSuccess
+        || cudaMemcpy(config[i].vec_b_device, b.data(), float_vec_size[i] * sizeof(float), cudaMemcpyHostToDevice)
+                   != cudaSuccess) {
+            throw std::runtime_error("Failed to copy data to device!");
+    }
+
+    gpu_execute.begin();
+
+    cuda_vector_add<<<blocks, threads>>>((float *) config[i].vec_a_device, (float *) config[i].vec_b_device,
+                                             config[i].step, float_vec_size[i], config[i].fix_position,
+                                             config[i].fix_step);
+
+    if (cudaDeviceSynchronize() != cudaSuccess) {
+    	throw std::runtime_error("Sync issue! (Launch failure?)");
+    }
+
+    gpu_execute.end();
+
+    if (cudaMemcpy(c.data(), config[i].vec_a_device, float_vec_size[i] * sizeof(float), cudaMemcpyDeviceToHost)
+            != cudaSuccess) {
+   		throw std::runtime_error("Could not copy data back!");
+    }
+
+    cudaFree(config[i].vec_a_device);
+    cudaFree(config[i].vec_b_device);
+		gpu_total.end();
+
+    std::cout << "GPU_" << config[i].device << " time: " << gpu_total.ms_elapsed()	
 
     return;
 }
