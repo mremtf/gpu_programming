65c88ddea0c775c16bd6e265d4587686984411eb
Will Starms <starmswa@canyon.rnet.missouri.edu>
2016-02-16 14:44:10 -0600

    script update

--- project_1/test_script.sh
+++ project_1/test_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 {
-echo "BLOCK,THREAD,MEM,OUTSTR"
+echo "BLOCK,THREAD,MEM,%TIME,TIME(units...)"
 for blk in 32 64 128 256 512 1024; do
   for thd in 32 64 128 256 512 1024; do
     for mem in 0.1 0.5 0.9; do
-      echo "${blk},${thd},${mem},$(nvprof ./vector_add -b ${blk} -t ${thd} -u ${mem} -v 2>&1 | fgrep cuda_vector_add)"
+      echo "${blk},${thd},${mem},$(vprof ./vector_add -b ${blk} -t ${thd} -u ${mem} -v 2>&1 | fgrep cuda_vector_add | xargs | cut -d' ' -f 1-2 --output-delimiter=',')"
     done
   done
 done
