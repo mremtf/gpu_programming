11276fdf220896f47a3ecf81e59ff20878031d24
mremtf <matt.r.england@gmail.com>
2016-02-17 14:25:58 -0600

    adding transpose analysis scripts

---
+++ project_1/transpose_gen_script_shared.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+{
+echo "BLOCK,THREAD,MEM,%TIME,TIME(units...)"
+for blk in 32 64 128 256 512 1024; do
+  for thd in 32 64 128 256 512 1024; do
+    for mem in 0.1 0.5 0.9; do
+      echo "${blk},${thd},${mem},$(nvprof ./transpose -b ${blk} -t ${thd} -u ${mem} -v 2>&1 | fgrep transpose_global | xargs | cut -d' ' -f 1-2 --output-delimiter=',')"
+    done
+  done
+done
+} > trans_g_vec_time.csv
