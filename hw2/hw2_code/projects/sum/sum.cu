fa36ed88685bc80d59e172cca60270076bffc83d
Will Starms <vilhelmen@gmail.com>
2016-03-09 16:27:07 -0600

    sum kernel stuff

--- hw2/hw2_code/projects/sum/sum.cu
+++ hw2/hw2_code/projects/sum/sum.cu
@@ -1,34 +1,34 @@
 /*
  * Copyright 1993-2006 NVIDIA Corporation.  All rights reserved.
  *
- * NOTICE TO USER:   
+ * NOTICE TO USER:
  *
- * This source code is subject to NVIDIA ownership rights under U.S. and 
- * international Copyright laws.  
+ * This source code is subject to NVIDIA ownership rights under U.S. and
+ * international Copyright laws.
  *
- * NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOURCE 
- * CODE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR 
- * IMPLIED WARRANTY OF ANY KIND.  NVIDIA DISCLAIMS ALL WARRANTIES WITH 
- * REGARD TO THIS SOURCE CODE, INCLUDING ALL IMPLIED WARRANTIES OF 
- * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.   
- * IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL, 
- * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS 
- * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE 
- * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE 
- * OR PERFORMANCE OF THIS SOURCE CODE.  
+ * NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOURCE
+ * CODE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR
+ * IMPLIED WARRANTY OF ANY KIND.  NVIDIA DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOURCE CODE, INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL,
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+ * OR PERFORMANCE OF THIS SOURCE CODE.
  *
- * U.S. Government End Users.  This source code is a "commercial item" as 
- * that term is defined at 48 C.F.R. 2.101 (OCT 1995), consisting  of 
- * "commercial computer software" and "commercial computer software 
- * documentation" as such terms are used in 48 C.F.R. 12.212 (SEPT 1995) 
- * and is provided to the U.S. Government only as a commercial end item.  
- * Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through 
- * 227.7202-4 (JUNE 1995), all U.S. Government End Users acquire the 
+ * U.S. Government End Users.  This source code is a "commercial item" as
+ * that term is defined at 48 C.F.R. 2.101 (OCT 1995), consisting  of
+ * "commercial computer software" and "commercial computer software
+ * documentation" as such terms are used in 48 C.F.R. 12.212 (SEPT 1995)
+ * and is provided to the U.S. Government only as a commercial end item.
+ * Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through
+ * 227.7202-4 (JUNE 1995), all U.S. Government End Users acquire the
  * source code with only those rights set forth herein.
  */
 
 #ifdef _WIN32
-#  define NOMINMAX 
+#define NOMINMAX
 #endif
 
 #define NUM_BANKS 16
@@ -48,80 +48,83 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 // declaration, forward
-void runTest( int argc, char** argv);
+void runTest(int argc, char **argv);
 
 // regression test functionality
-extern "C" 
-unsigned int compare( const float* reference, const float* data, 
-                      const unsigned int len);
-extern "C" 
-void computeGold( float* reference, float* idata, const unsigned int len);
+extern "C" unsigned int compare(const float *reference, const float *data, const unsigned int len);
+extern "C" void computeGold(float *reference, float *idata, const unsigned int len);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Program main
 ////////////////////////////////////////////////////////////////////////////////
-int 
-main( int argc, char** argv) 
-{
-    runTest( argc, argv);
+int main(int argc, char **argv) {
+    runTest(argc, argv);
     CUT_EXIT(argc, argv);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 //! Run a scan test for CUDA
 ////////////////////////////////////////////////////////////////////////////////
-void
-runTest( int argc, char** argv) 
-{
+void runTest(int argc, char **argv) {
     CUT_DEVICE_INIT();
 
     int num_elements = 2;
-    cutGetCmdLineArgumenti( argc, (const char**) argv, "n", &num_elements);
+    cutGetCmdLineArgumenti(argc, (const char **) argv, "n", &num_elements);
 
     unsigned int timer;
-    CUT_SAFE_CALL( cutCreateTimer(&timer));
-    
-    const unsigned int num_threads = 2;
-    const unsigned int mem_size = sizeof( float) * num_elements;
+    CUT_SAFE_CALL(cutCreateTimer(&timer));
 
-    float* h_data = (float*) malloc( mem_size);
-      
-    printf("INPUT: ");
-    for( unsigned int i = 0; i < num_elements; ++i) 
-    {
-        h_data[i] = floorf(1000*(rand()/(float)RAND_MAX));
-        printf(" %f ", h_data[i]);
+    // Nope, 32. Always and forever.
+    // const unsigned int num_threads = 2;
+
+    // lazy re-adjust because we need it to be a multiple of 32 or we'll explode
+    int correct_size = num_elements;
+    if (correct_size % 32) {
+        correct_size = (correct_size / 32 + 1) * 32;
     }
-    printf("\n");
+    unsigned int block_count = correct_size / 32;
 
-    float* reference = (float*) malloc( mem_size);  
-    computeGold( reference, h_data, num_elements);
+    const unsigned int mem_size = sizeof(float) * correct_size;
 
-    float* d_idata;
-    float* d_odata;
-    CUDA_SAFE_CALL( cudaMalloc( (void**) &d_idata, mem_size));
-    CUDA_SAFE_CALL( cudaMalloc( (void**) &d_odata, mem_size));
+    float *h_data = (float *) malloc(mem_size);
 
-    CUDA_SAFE_CALL( cudaMemcpy( d_idata, h_data, mem_size, cudaMemcpyHostToDevice) );
+    // printf("INPUT: ");
+    for (unsigned int i = 0; i < num_elements; ++i) {
+        h_data[i] = floorf(1000 * (rand() / (float) RAND_MAX));
+        // printf(" %f ", h_data[i]);
+    }
+    // blank out extras
+    for (unsigned int i = num_elements; i < correct_size; ++i){
+        h_data[i] = 0.0;
+    }
+    // printf("\n");
 
-#ifndef __DEVICE_EMULATION__
-    dim3  grid(1, 1, 1);  
-#else
-    dim3  grid(1, 1, 1); // only one run block in device emu mode or it will be too slow
-#endif
-    dim3  threads(num_threads*2, 1, 1);
+    float *reference = (float *) malloc(mem_size);
+    computeGold(reference, h_data, num_elements);
+
+    float *d_idata;
+    float *d_odata;
+    CUDA_SAFE_CALL(cudaMalloc((void **) &d_idata, mem_size));
+    CUDA_SAFE_CALL(cudaMalloc((void **) &d_odata, sizeof(float)));
+
+    // sending a literal zero down because I don't want to memset it to 0x00 because
+    // what if that is wrong for some reason.
+    // AND I can't just have global id 0 blank it because it might get scheduled weird
+    // It's dumb but deal with it.
+    const float literal_zero = 0.0f;
+    CUDA_SAFE_CALL(cudaMemcpy(d_odata,&literal_zero,cudaMemcpyHostToDevice));
+
+    CUDA_SAFE_CALL(cudaMemcpy(d_idata, h_data, mem_size, cudaMemcpyHostToDevice));
 
     CUT_CHECK_ERROR("Kernel execution failed");
 
     printf("Running sum of %d elements\n", num_elements);
-  
+
     unsigned int numIterations = 100;
 
     cutStartTimer(timer);
-    for (int i = 0; i < numIterations; ++i)
-    {
-        sum_kernel<<< grid, threads, 2 * mem_size >>>
-            (d_odata, d_idata, num_elements);
+    for (int i = 0; i < numIterations; ++i) {
+        sum_kernel<<<block_count, 32, sizeof(float) * 32>>>(d_odata, d_idata);
     }
     cudaThreadSynchronize();
     cutStopTimer(timer);
@@ -131,8 +134,7 @@ runTest( int argc, char** argv)
 
     CUT_CHECK_ERROR("Kernel execution failed");
 
-    CUDA_SAFE_CALL(cudaMemcpy( h_data, d_odata, sizeof(float) * num_elements, 
-                                   cudaMemcpyDeviceToHost));
+    CUDA_SAFE_CALL(cudaMemcpy(h_data, d_odata, sizeof(float), cudaMemcpyDeviceToHost));
 
     printf("OUTPUT: ");
     printf(" %f ", h_data[0]);
@@ -143,15 +145,15 @@ runTest( int argc, char** argv)
 
     // custom output handling when no regression test running
     // in this case check if the result is equivalent to the expected soluion
-    
-    // We can use an epsilon of 0 since values are integral and in a range 
-    // that can be exactly represented
-    float epsilon = 0.0f;
-    unsigned int result_regtest = cutComparefe( reference, h_data, 1, epsilon);
-    printf( "sum: Test %s\n", (1 == result_regtest) ? "PASSED" : "FAILED");
 
-    free( h_data);
-    free( reference);
+    // We can use an epsilon of 0 since values are integral and in a range
+    // that can be exactly represented
+    float epsilon               = 0.0f;
+    unsigned int result_regtest = cutComparefe(reference, h_data, 1, epsilon);
+    printf("sum: Test %s\n", (1 == result_regtest) ? "PASSED" : "FAILED");
+
+    free(h_data);
+    free(reference);
     CUDA_SAFE_CALL(cudaFree(d_idata));
     CUDA_SAFE_CALL(cudaFree(d_odata));
     CUT_SAFE_CALL(cutDeleteTimer(timer));
--- hw2/hw2_code/projects/sum/sum_kernel.cu
+++ hw2/hw2_code/projects/sum/sum_kernel.cu
@@ -1,29 +1,29 @@
 /*
  * Copyright 1993-2006 NVIDIA Corporation.  All rights reserved.
  *
- * NOTICE TO USER:   
+ * NOTICE TO USER:
  *
- * This source code is subject to NVIDIA ownership rights under U.S. and 
- * international Copyright laws.  
+ * This source code is subject to NVIDIA ownership rights under U.S. and
+ * international Copyright laws.
  *
- * NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOURCE 
- * CODE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR 
- * IMPLIED WARRANTY OF ANY KIND.  NVIDIA DISCLAIMS ALL WARRANTIES WITH 
- * REGARD TO THIS SOURCE CODE, INCLUDING ALL IMPLIED WARRANTIES OF 
- * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.   
- * IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL, 
- * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS 
- * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE 
- * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE 
- * OR PERFORMANCE OF THIS SOURCE CODE.  
+ * NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOURCE
+ * CODE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR
+ * IMPLIED WARRANTY OF ANY KIND.  NVIDIA DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOURCE CODE, INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL,
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+ * OR PERFORMANCE OF THIS SOURCE CODE.
  *
- * U.S. Government End Users.  This source code is a "commercial item" as 
- * that term is defined at 48 C.F.R. 2.101 (OCT 1995), consisting  of 
- * "commercial computer software" and "commercial computer software 
- * documentation" as such terms are used in 48 C.F.R. 12.212 (SEPT 1995) 
- * and is provided to the U.S. Government only as a commercial end item.  
- * Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through 
- * 227.7202-4 (JUNE 1995), all U.S. Government End Users acquire the 
+ * U.S. Government End Users.  This source code is a "commercial item" as
+ * that term is defined at 48 C.F.R. 2.101 (OCT 1995), consisting  of
+ * "commercial computer software" and "commercial computer software
+ * documentation" as such terms are used in 48 C.F.R. 12.212 (SEPT 1995)
+ * and is provided to the U.S. Government only as a commercial end item.
+ * Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through
+ * 227.7202-4 (JUNE 1995), all U.S. Government End Users acquire the
  * source code with only those rights set forth herein.
  */
 
@@ -34,7 +34,7 @@
 //! Work-efficient compute implementation of scan, one thread per 2 elements
 //! Work-efficient: O(log(n)) steps, and O(n) adds.
 //! Also shared storage efficient: Uses n elements in shared mem -- no ping-ponging
-//! Uses a balanced tree type algorithm.  See Blelloch, 1990 "Prefix Sums 
+//! Uses a balanced tree type algorithm.  See Blelloch, 1990 "Prefix Sums
 //! and Their Applications", or Prins and Chatterjee PRAM course notes:
 //! http://www.cs.unc.edu/~prins/Classes/203/Handouts/pram.pdf
 //!
@@ -45,22 +45,46 @@
 //! @param g_idata  input data in global memory
 //! @param n        input number of elements to scan from input data
 ///////////////////////////////////////////////////////////////////////////////
-__global__ void sum_kernel(float *g_odata, float *g_idata, int n)
-{
-  
-  extern  __shared__  float sdata[];
+__global__ void sum_kernel(float *g_odata, float *g_idata, int n) {
+    extern __shared__ float sdata[];
 
-  const unsigned int tid = threadIdx.x;
+    const unsigned int tid = threadIdx.x;
 
-  sdata[2*tid] = g_idata[2*tid];
-  sdata[2*tid+1] = g_idata[2*tid+1];
-  __syncthreads();
+    sdata[2 * tid]     = g_idata[2 * tid];
+    sdata[2 * tid + 1] = g_idata[2 * tid + 1];
+    __syncthreads();
 
-  sdata[2*tid] = sdata[2*tid] + sdata[2*tid+1];
-  __syncthreads();
+    sdata[2 * tid] = sdata[2 * tid] + sdata[2 * tid + 1];
+    __syncthreads();
 
-  g_odata[tid]   = sdata[tid];
-
+    g_odata[tid] = sdata[tid];
 }
 
-#endif // #ifndef _SCAN_WORKEFFICIENT_KERNEL_H_
+__global__ void sum_kernel(float *global_out, float *global_in) {
+    // finally get to use the reduction code form HPC
+    extern __shared__ float shared_data[];
+
+    const unsigned int tid = threadIdx.x;
+    const unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Writing it such that we always round up to the nearest multiple of the warp size (32)
+    // And the extra is set to zero, so we can just do it and not have any issues
+    // So ALL threads are working in valid memory
+
+    shared_data[tid] = global_in[gid];
+    __syncthreads();
+
+    for (unsigned int cutoff = blockDim.x >> 1; cutoff > 0; cutoff >>= 1) {
+        if (tid < cutoff) {
+            shared_data[tid] += shared_data[tid + cutoff];
+        }
+    }
+
+    if (tid = 0) {
+        // atomically add our block results to the output float
+        // shared -> register -> global surely won't be faster than shared->global
+        atomicAdd(global_out,shared_data[0]);
+    }
+}
+
+#endif  // #ifndef _SCAN_WORKEFFICIENT_KERNEL_H_
