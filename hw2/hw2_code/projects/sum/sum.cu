f71fa2595408893a05de99fc7ba6939fdca170a8
Will Starms <starmswa@canyon.rnet.missouri.edu>
2016-03-13 21:12:59 -0500

    finished sum kernel

--- hw2/hw2_code/projects/sum/sum.cu
+++ hw2/hw2_code/projects/sum/sum.cu
@@ -1,32 +1,3 @@
-/*
- * Copyright 1993-2006 NVIDIA Corporation.  All rights reserved.
- *
- * NOTICE TO USER:
- *
- * This source code is subject to NVIDIA ownership rights under U.S. and
- * international Copyright laws.
- *
- * NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOURCE
- * CODE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR
- * IMPLIED WARRANTY OF ANY KIND.  NVIDIA DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOURCE CODE, INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL,
- * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
- * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
- * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
- * OR PERFORMANCE OF THIS SOURCE CODE.
- *
- * U.S. Government End Users.  This source code is a "commercial item" as
- * that term is defined at 48 C.F.R. 2.101 (OCT 1995), consisting  of
- * "commercial computer software" and "commercial computer software
- * documentation" as such terms are used in 48 C.F.R. 12.212 (SEPT 1995)
- * and is provided to the U.S. Government only as a commercial end item.
- * Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through
- * 227.7202-4 (JUNE 1995), all U.S. Government End Users acquire the
- * source code with only those rights set forth herein.
- */
-
 #ifndef LOAD_LEVEL
 #define LOAD_LEVEL 1
 #endif
@@ -101,16 +72,16 @@ void runTest(int argc, char **argv) {
 
     float *h_data = (float *) malloc(mem_size);
 
-    // printf("INPUT: ");
+    //printf("INPUT: ");
     for (unsigned int i = 0; i < num_elements; ++i) {
         h_data[i] = floorf(10 * (rand() / (float) RAND_MAX));
-        // printf(" %f ", h_data[i]);
+        //printf(" %f ", h_data[i]);
     }
     // blank out extras
     for (unsigned int i = num_elements; i < correct_size; ++i) {
         h_data[i] = 0.0f;
     }
-    // printf("\n");
+    //printf("\n");
 
     float *reference = (float *) malloc(mem_size);
     computeGold(reference, h_data, num_elements);
--- hw2/hw2_code/projects/sum/sum_kernel.cu
+++ hw2/hw2_code/projects/sum/sum_kernel.cu
@@ -1,50 +1,135 @@
-/*
- * Copyright 1993-2006 NVIDIA Corporation.  All rights reserved.
- *
- * NOTICE TO USER:
- *
- * This source code is subject to NVIDIA ownership rights under U.S. and
- * international Copyright laws.
- *
- * NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOURCE
- * CODE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR
- * IMPLIED WARRANTY OF ANY KIND.  NVIDIA DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOURCE CODE, INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL,
- * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
- * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
- * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
- * OR PERFORMANCE OF THIS SOURCE CODE.
- *
- * U.S. Government End Users.  This source code is a "commercial item" as
- * that term is defined at 48 C.F.R. 2.101 (OCT 1995), consisting  of
- * "commercial computer software" and "commercial computer software
- * documentation" as such terms are used in 48 C.F.R. 12.212 (SEPT 1995)
- * and is provided to the U.S. Government only as a commercial end item.
- * Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through
- * 227.7202-4 (JUNE 1995), all U.S. Government End Users acquire the
- * source code with only those rights set forth herein.
- */
-
 #ifndef _SCAN_WORKEFFICIENT_KERNEL_H_
 #define _SCAN_WORKEFFICIENT_KERNEL_H_
 
-///////////////////////////////////////////////////////////////////////////////
-//! Work-efficient compute implementation of scan, one thread per 2 elements
-//! Work-efficient: O(log(n)) steps, and O(n) adds.
-//! Also shared storage efficient: Uses n elements in shared mem -- no ping-ponging
-//! Uses a balanced tree type algorithm.  See Blelloch, 1990 "Prefix Sums
-//! and Their Applications", or Prins and Chatterjee PRAM course notes:
-//! http://www.cs.unc.edu/~prins/Classes/203/Handouts/pram.pdf
-//!
-//! Pro: Work Efficient
-//! Con: Shared memory bank conflicts due to the addressing used.
-//
-//! @param g_odata  output data in global memory
-//! @param g_idata  input data in global memory
-//! @param n        input number of elements to scan from input data
-///////////////////////////////////////////////////////////////////////////////
+#ifndef LOAD_LEVEL
+#error "LOAD_LEVEL not defined, something probably went terribly wrong during compilation"
+#endif
+
+#define DATA_READ_OFFSET (32 >> (LOAD_LEVEL - 1))
+#if DATA_READ_OFFSET == 0
+#error "Load level too high!"
+#endif
+
+#define DATA_READ_LOAD (1 << (LOAD_LEVEL - 1))
+
+#define COMPUTATION_LOAD_COUNT ((1 << LOAD_LEVEL) - 1)
+
+__global__ void sum_kernel(float *global_out, float *global_in) {
+    // finally get to use the reduction code form HPC
+    extern __shared__ float sdata[];
+
+    float *shared_data = sdata;
+
+    const unsigned int tid = threadIdx.x;
+    const unsigned int gid = blockIdx.x * 32 /*blockDim.x*/ + threadIdx.x;
+
+    // Writing it such that we always round up to the nearest multiple of the warp size (32)
+    // And the extra is set to zero, so we can just do it and not have any issues
+    // So ALL threads are working in valid memory
+    global_in += gid;
+
+    // Offsets can be calculated of the blockdim, since the blockdim has been set by the load level
+    // 32 / blockDim.x is effectively LOAD_LEVEL
+
+    shared_data = sdata + tid;
+    for (int i = 0; i < DATA_READ_LOAD; ++i, global_in += DATA_READ_OFFSET, shared_data += DATA_READ_OFFSET) {
+//printf("%d_%d_%d LOAD %f TO %d\n",tid,blockIdx.x,gid,*global_in,shared_data - sdata);
+        *shared_data = *global_in;
+    }
+    // There are no other warps, so no need to sync
+    //__syncthreads();
+
+    shared_data = sdata + tid;
+
+    for (unsigned int i = 0; i < DATA_READ_LOAD; ++i, shared_data += DATA_READ_OFFSET){
+    for (unsigned int cutoff = blockDim.x >> 1; cutoff > 0; cutoff >>= 1) {
+        if (tid < cutoff) {
+//float dbg = *shared_data;
+//float dbg_2 = shared_data[cutoff];
+//printf("%d_%d_%d PASS CUTOFF %d\n",tid,blockIdx.x,gid,cutoff);
+//printf("%d_%d_%d ADDED %d (%f) AND %d (%f)\n",tid,blockIdx.x,gid,tid+i*DATA_READ_OFFSET,dbg,tid+(i*DATA_READ_OFFSET) + cutoff,dbg_2);
+            *shared_data += shared_data[cutoff];
+        }
+        //__syncthreads();
+    }
+        if (i != 0 && tid == 0) {
+//printf("%d_%d_%d SAVE PARTIAL RESULT %f\n",tid,blockIdx.x,gid,*shared_data);
+            sdata[0] += *shared_data;
+        }
+    }
+
+    if (tid == 0) {
+        // atomically add our block results to the output float
+        // shared -> register -> global surely won't be faster than shared->global
+        atomicAdd(global_out, sdata[0]);
+    }
+}
+
+/*
+YOU WERE TOO GOOD FOR THIS WORLD
+
+Basically, it COMPLETELY corrected the work load to a basic reduction after one iteration
+But I kinda assumed it would have to be corrected every iteration
+So it tried to "help" with the others and inflates the sum
+
+And I can't think of a better way to make the first iteration special without
+just putting another if in the loop and that's gross
+
+So I'm just going to dumb it down and make it do the smaller reduction multiple times.
+
+It's slightly less efficient, but I've already made it less efficient by
+making the threads adjustable anyway because that's a requirement :/
+
+__global__ void sum_kernel(float *global_out, float *global_in) {
+    // finally get to use the reduction code form HPC
+    extern __shared__ float sdata[];
+
+    float *shared_data = sdata;
+
+    const unsigned int tid = threadIdx.x;
+    const unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Writing it such that we always round up to the nearest multiple of the warp size (32)
+    // And the extra is set to zero, so we can just do it and not have any issues
+    // So ALL threads are working in valid memory
+
+    global_in += gid;
+
+    // Offsets can be calculated of the blockdim, since the blockdim has been set by the load level
+    // 32 / blockDim.x is effectively LOAD_LEVEL
+
+    shared_data = sdata + tid;
+    for (int i = 0; i < DATA_READ_LOAD; ++i, global_in += DATA_READ_OFFSET, shared_data += DATA_READ_OFFSET) {
+printf("%d LOAD %f INTO POSITION %d\n",tid,*global_in,shared_data - sdata);
+        *shared_data = *global_in;
+    }
+    //__syncthreads();
+
+    shared_data = sdata + tid;
+
+    for (unsigned int cutoff = blockDim.x >> 1; cutoff > 0; cutoff >>= 1) {
+        if (tid < cutoff) {
+printf("TID %d MADE IT TO CUTOFF %d\n",tid,cutoff);
+            // shared_data[tid] += shared_data[tid + cutoff];
+            float p_sum          = 0.0f;
+            float *shared_offset = shared_data + cutoff;
+            for (int i = 0; i < COMPUTATION_LOAD_COUNT; ++i, shared_offset += cutoff) {
+                p_sum += shared_data[(i+1)*cutoff];
+printf("%d ADDED POSITION %d (%f) FOR %f\n",tid,(i+1)*cutoff+tid,shared_data[(i+1)*cutoff],p_sum);
+            }
+            *shared_data += p_sum;
+printf("%d ADD ALL TO BASE %f (+ %f)\n",tid,*shared_data,p_sum);
+        }
+        //__syncthreads();
+    }
+
+    if (tid == 0) {
+        // atomically add our block results to the output float
+        // shared -> register -> global surely won't be faster than shared->global
+        atomicAdd(global_out, *shared_data);
+    }
+}
+*/
 /*
 No. This is gross, and I can't guarentee it'll work everywhere all the time.
 Something just feels WRONG about this idea and I can't put my finger on it.
@@ -80,62 +165,6 @@ __global__ void sum_kernel(float *result, float *data, const unsigned n, const u
     }
 }
 */
-
-#ifndef LOAD_LEVEL
-#error "LOAD_LEVEL not defined, something probably went terribly wrong during compilation"
-#endif
-
-#define DATA_LOAD_OFFSET (32 >> (LOAD_LEVEL - 1))
-#if DATA_LOAD_OFFSET == 0
-#error "Load level too high!"
-#endif
-
-#define COMPUTATION_LOAD_COUNT ((1 << LOAD_LEVEL) - 1)
-
-__global__ void sum_kernel(float *global_out, float *global_in) {
-    // finally get to use the reduction code form HPC
-    extern __shared__ float sdata[];
-
-    float *shared_data = sdata;
-
-    const unsigned int tid = threadIdx.x;
-    const unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
-
-    // Writing it such that we always round up to the nearest multiple of the warp size (32)
-    // And the extra is set to zero, so we can just do it and not have any issues
-    // So ALL threads are working in valid memory
-
-    global_in += gid;
-
-    // Offsets can be calculated of the blockdim, since the blockdim has been set by the load level
-    // 32 / blockDim.x is effectively LOAD_LEVEL
-
-    for (int i = 0; i < LOAD_LEVEL; ++i, global_in += DATA_LOAD_OFFSET) {
-        shared_data[tid] = *global_in;
-    }
-    //__syncthreads();
-
-    shared_data = shared_data + tid;
-
-    for (unsigned int cutoff = blockDim.x >> 1; cutoff > 0; cutoff >>= 1) {
-        if (tid < cutoff) {
-            // shared_data[tid] += shared_data[tid + cutoff];
-            float p_sum          = 0.0f;
-            float *shared_offset = shared_data + cutoff;
-            for (int i = 0; i < COMPUTATION_LOAD_COUNT; ++i, shared_offset += cutoff) {
-                p_sum += *shared_offset;
-            }
-            *shared_data += p_sum;
-        }
-        //__syncthreads();
-    }
-
-    if (tid == 0) {
-        // atomically add our block results to the output float
-        // shared -> register -> global surely won't be faster than shared->global
-        atomicAdd(global_out, *shared_data);
-    }
-}
 
 
 #endif  // #ifndef _SCAN_WORKEFFICIENT_KERNEL_H_
